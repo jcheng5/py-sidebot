518fd0a4d62b28ec3e68eda1c2ce785c5c6006e1
Joe Cheng <joe@posit.co>
2024-07-31 18:26:25 -0700

    Turn off community extensions for duckdb

--- shared.py
+++ shared.py
@@ -3,6 +3,8 @@ from pathlib import Path
 import duckdb
 import pandas as pd
 
+duckdb.query("SET allow_community_extensions = false;")
+
 here = Path(__file__).parent
 tips = pd.read_csv(here / "tips.csv")
 tips["percent"] = tips.tip / tips.total_bill
